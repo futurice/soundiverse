cd2319bf296f3185abbe749fc9ca296b3deb0125
Olli Niskanen <olli.niskanen@futurice.com>
2016-04-01 11:06:30 +0300

    Prototype version of console frame ticker

--- src/Main.purs
+++ src/Main.purs
@@ -5,19 +5,26 @@ import Prelude
 import Data.Maybe
 
 import Control.Monad.Eff
-import Control.Monad.Eff.Console (error)
+import Control.Monad.Eff.Console (error, log)
 
 import Graphics.Canvas (getCanvasElementById, getContext2D)
 import Graphics.Canvas.Free
 
+import Signal
+import Signal.DOM
+
+--main = do
+--  canvas <- getCanvasElementById "canvas"
+--  case canvas of
+--    Nothing -> error "No canvas"
+--    Just canvasElement -> do
+--      context <- getContext2D canvasElement
+--      runGraphics context $ do
+--        -- Canvas API calls will go here
+--        setFillStyle "#AAB333"
+--        rect { x: 0.0, y: 0.0, w: 400.0, h: 400.0 }
+--        fill
+
 main = do
-  canvas <- getCanvasElementById "canvas"
-  case canvas of
-    Nothing -> error "No canvas"
-    Just canvasElement -> do
-      context <- getContext2D canvasElement
-      runGraphics context $ do
-        -- Canvas API calls will go here
-        setFillStyle "#AAB333"
-        rect { x: 0.0, y: 0.0, w: 400.0, h: 400.0 }
-        fill
+  frame <- animationFrame
+  runSignal (log <$> (show <$> frame))
