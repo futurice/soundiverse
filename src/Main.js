bff388885334b7eb5238c882e2a78b812d0c239a
David Sheldrick <david@futurice.com>
2016-04-02 09:08:26 +0300

    audio signal working kinda

--- src/Main.js
+++ src/Main.js
@@ -1,204 +1,45 @@
 // module Main
 
-exports.getFFTSize = function (analyser)
-{
-	return function() {
-		console.log("JS: ", analyser.fftSize);
-		return analyser.fftSize;
-	}
-	
-}
+exports.audioStreamP =
+  function audioStreamP(constant) {
 
-exports.getByteFrequencyData = function (analyser)
-{
-	return function() {
-		var array = new Uint8Array(analyser.frequencyBinCount);
-		console.log("JS: Getting byte frequency data");
-		analyser.getByteFrequencyData(array);
-		return array;
-	}
-}
+		// Create context + analyser
+	  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+	  var analyser = audioCtx.createAnalyser();
+	  analyser.fftSize = 4096;
 
-exports.fillArray = function (value, len) {
-  if (len == 0) return [];
-  var a = [value];
-  while (a.length * 2 <= len) a = a.concat(a);
-  if (a.length < len) a = a.concat(a.slice(0, len - a.length));
-  return a;
-}
+		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+		
 
-exports.constNum = function()
-{
-	return 1;
-}
+	  // Connect microphone to analyser
+	  navigator.getUserMedia(
+			{audio: true},
+	    connect,
+	    () => console.error('doh')
+		);
 
-exports.constArray = function()
-{
-	console.log("Here");
- 	return [1, 2, 3];
-}
+		var dataArray = new Uint8Array(0);
+		var out = constant(dataArray);
 
-exports.onDOMContentLoaded =
-  function onDOMContentLoaded(action) {
+		function connect (mediaStream) {
+			var source = audioCtx.createMediaStreamSource(mediaStream);
+			source.connect(analyser);
+
+			var bufferLength = analyser.frequencyBinCount;
+
+
+			function feedSignal () {
+			  dataArray = new Uint8Array(bufferLength);
+				analyser.getByteFrequencyData(dataArray);
+
+				out.set(dataArray);
+				window.requestAnimationFrame(feedSignal);
+			}
+			window.requestAnimationFrame(feedSignal);
+		}
+
+
     return function() {
-      if (document.readyState === "interactive") {
-        action();
-      } else {
-        document.addEventListener("DOMContentLoaded", action);
-      }
-      return {};
+      return out;
     };
   };
-
-exports.getAudioContext = function()
-{
-	console.log("Getting audio context");
-	var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-	return audioCtx;
-	
-}
-
-exports.getAnalyserNode = function(ctx)
-{
-	return function() {
-		console.log("Getting analyser");
-		return ctx.createAnalyser();	
-	}
-	
-}
-
-exports.microphoneInputFrameP =
-  function microphoneInputFrameP(constant) {
-  	return function(context) {
-	    return function(analyser) {
-		    return function() {
-		  	  	navigator.getUserMedia({audio: true},
-			    connectMediaStreamToAnalyser(constant, context, analyser, drawOntoCanvas),
-			    noUserMedia);
-		    };
-	    };
-  	};
-};
-
-var WIDTH = 400;
-var HEIGHT = 300;
-
-function drawOntoCanvas(constant, analyser){
-    var requestAnimFrame, cancelAnimFrame;
-    if (window.requestAnimationFrame) {
-      requestAnimFrame = window.requestAnimationFrame;
-      cancelAnimFrame = window.cancelAnimationFrame;
-    } else if (window.mozRequestAnimationFrame) {
-      requestAnimFrame = window.mozRequestAnimationFrame;
-      cancelAnimFrame = window.mozCancelAnimationFrame;
-    } else if (window.webkitRequestAnimationFrame) {
-      requestAnimFrame = window.webkitRequestAnimationFrame;
-      cancelAnimFrame = window.webkitCancelAnimationFrame;
-    } else if (window.msRequestAnimationFrame) {
-      requestAnimFrame = window.msRequestAnimationFrame;
-      cancelAnimFrame = window.msCancelAnimationFrame;
-    } else if (window.oRequestAnimationFrame) {
-      requestAnimFrame = window.oRequestAnimationFrame;
-      cancelAnimFrame = window.oCancelAnimationFrame;
-    } else {
-      requestAnimFrame = function(cb) {setTimeout(function() {cb(now())}, 1000/60)};
-      cancelAnimFrame = window.clearTimeout;
-    }
-
-    function getData(a){
-        var array = new Uint8Array(a.frequencyBinCount);
-		console.log("JS: Getting byte frequency data");
-		a.getByteFrequencyData(array);
-		return array;
-	}
-
-	var out = constant(getData(analyser))
-    requestAnimFrame(function tick(t) {
-      out.set(t); requestAnimFrame(tick);
-    });
-    return out;
-};
-
-function ready(fn) {
-  if (document.readyState != 'loading'){
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
-}
-
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-function noUserMedia() {
-  console.error("User did not allow media");
-}
-
-function connectMediaStreamToAnalyser(constant, audioCtx, analyser, callback) {
-  return function(mediaStream) {
-
-    var source = audioCtx.createMediaStreamSource(mediaStream);
-    source.connect(analyser);
-    callback(constant, analyser);
-  }
-}
-
-function drawOntoCanvas(analyser) {
-  var bufferLength = analyser.frequencyBinCount;
-  var canvas = document.getElementById("canvas");
-  var canvasCtx = canvas.getContext('2d');
-  var dataArray = new Uint8Array(bufferLength);
-
-  function drawAudioData() {
-    requestAnimationFrame(drawAudioData);
-    analyser.getByteFrequencyData(dataArray);
-
-    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-    canvasCtx.lineWidth = 2;
-    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
-    canvasCtx.beginPath();
-    var sliceWidth = WIDTH * 1.0 / bufferLength;
-    var x = 0;
-
-    for(var i = 0; i < bufferLength; i++) {
-
-      var v = dataArray[i] / 128.0;
-      var y = v * HEIGHT/2;
-
-      if(i === 0) {
-        canvasCtx.moveTo(x, y);
-      } else {
-        canvasCtx.lineTo(x, y);
-      }
-
-      x += sliceWidth;
-    }
-    canvasCtx.lineTo(canvas.width, canvas.height/2);
-    canvasCtx.stroke();
-  };
-
-  drawAudioData();
-}
-
-
-// ready(function() {
-
-//   var canvas = document.getElementById("canvas");
-//   var canvasCtx = canvas.getContext('2d');
-//   canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
-
-
-//   if (!navigator.getUserMedia) {
-//     console.error("No support for getUserMedia");
-//     return;
-//   }
-
-//   // Create context + analyser
-//   var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-//   var analyser = audioCtx.createAnalyser();
-//   analyser.fftSize = 4096;
-
-//   // Connect microphone to analyser
-
-
-// });
--- src/Main.purs
+++ src/Main.purs
@@ -1,14 +1,14 @@
 module Main where
 
-import Prelude (Unit, show, (<$>), bind, map)
+import Prelude
 
 import Data.Maybe
+import Data.ArrayBuffer.Types (Uint8Array)
 
 import Control.Timer
 import Control.Monad.Eff
 import Control.Monad.Eff.Random
 import Control.Monad.Eff.Console
-import DOM(DOM())
 
 import Graphics.Canvas (getCanvasElementById, getContext2D)
 import Graphics.Canvas.Free
@@ -16,6 +16,8 @@ import Graphics.Canvas.Free
 import Signal (Signal, runSignal, constant)
 import Signal.DOM (animationFrame)
 import Signal.Time (Time)
+
+foreign import data AUDIO :: !
 
 --main = do
 --  canvas <- getCanvasElementById "canvas"
@@ -33,21 +35,21 @@ import Signal.Time (Time)
 
 --foreign import fillArray :: Int -> Int -> Array Int
 
-foreign import constArray :: forall eff. Eff (console :: CONSOLE | eff) (Array Int)
-foreign import onDOMContentLoaded :: forall a eff. Eff (dom :: DOM | eff) a -> Eff (eff) Unit
-
-foreign import data WebAudio :: !
-foreign import data AudioContext :: *
-foreign import data AnalyserNode :: *
-
-foreign import getAudioContext :: forall wau eff. Eff (wau :: WebAudio, console :: CONSOLE | eff) AudioContext
-
-foreign import getAnalyserNode :: forall wau eff. AudioContext -> (Eff (wau :: WebAudio, console :: CONSOLE | eff) AnalyserNode)
-
-foreign import getFFTSize
-  :: forall wau. AnalyserNode -> (Eff (wau :: WebAudio, console :: CONSOLE, dom :: DOM) Int)
-
-foreign import getByteFrequencyData :: forall wau. AnalyserNode -> (Eff (wau :: WebAudio, console :: CONSOLE, dom :: DOM) (Array Int))
+-- foreign import constArray :: forall eff. Eff (console :: CONSOLE | eff) (Array Int)
+-- foreign import onDOMContentLoaded :: forall a eff. Eff (dom :: DOM | eff) a -> Eff (eff) Unit
+--
+-- foreign import data WebAudio :: !
+-- foreign import data AudioContext :: *
+-- foreign import data AnalyserNode :: *
+--
+-- foreign import getAudioContext :: forall wau eff. Eff (wau :: WebAudio, console :: CONSOLE | eff) AudioContext
+--
+-- foreign import getAnalyserNode :: forall wau eff. AudioContext -> (Eff (wau :: WebAudio, console :: CONSOLE | eff) AnalyserNode)
+--
+-- foreign import getFFTSize
+--   :: forall wau. AnalyserNode -> (Eff (wau :: WebAudio, console :: CONSOLE, dom :: DOM) Int)
+--
+-- foreign import getByteFrequencyData :: forall wau. AnalyserNode -> (Eff (wau :: WebAudio, console :: CONSOLE, dom :: DOM) (Array Int))
 
 --constString :: String -> String
 --constString a = a
@@ -58,21 +60,39 @@ foreign import getByteFrequencyData :: forall wau. AnalyserNode -> (Eff (wau :: 
 --constArray :: Array Int
 --constArray = [1, 2]
 
-type MicrophoneSignal = Eff (dom :: DOM, timer :: TIMER) (Signal (Array Int))
+-- type MicrophoneSignal = Eff (dom :: DOM, timer :: TIMER) (Signal (Array Int))
+--
+-- foreign import microphoneInputFrameP :: forall e c. (c -> Signal c) -> AudioContext -> Eff (wau :: WebAudio, dom :: DOM, timer :: TIMER | e) Time -> MicrophoneSignal
+--
+-- microphoneInputFrame :: forall wau e. AudioContext -> Eff (wau :: WebAudio, dom :: DOM, timer :: TIMER | e) Time -> MicrophoneSignal
+-- microphoneInputFrame audioCtx = microphoneInputFrameP constant audioCtx
+--
+-- --type FrameSignal = Eff (dom :: DOM, timer :: TIMER) (Signal Time)
+--
+-- logshow :: forall e a. Show a => a -> Eff (console :: CONSOLE | e) Unit
+-- logshow = log <<< show
+--
+-- maplogshow :: forall a e. Show a => Signal a -> Signal (Eff (console :: CONSOLE | e) Unit)
+-- maplogshow = map logshow
+--
+--
+-- --main :: forall t19. Eff ( timer :: TIMER, console :: CONSOLE | t19) Unit
+-- main = onDOMContentLoaded do
+-- 	ctx <- getAudioContext
+-- 	-- analyser <- getAnalyserNode ctx
+-- 	inputStream <- microphoneInputFrame ctx
+-- 	runSignal $ maplogshow inputStream
+-- 	-- runSignal (map log stringified)
+--   	--frame <- animationFrame
+--   	--runSignal (map log (map show frame))
 
-foreign import microphoneInputFrameP :: forall e c. (c -> Signal c) -> AudioContext -> Eff (wau :: WebAudio, dom :: DOM, timer :: TIMER | e) Time -> MicrophoneSignal
+foreign import audioStreamP :: forall e c. (c -> Signal c) -> Eff (audio :: AUDIO | e) (Signal String)
 
-microphoneInputFrame :: forall wau e. AudioContext -> Eff (wau :: WebAudio, dom :: DOM, timer :: TIMER | e) Time -> MicrophoneSignal
-microphoneInputFrame audioCtx = microphoneInputFrameP constant audioCtx
+audioStream :: forall e. Eff (audio :: AUDIO | e) (Signal String)
+audioStream = audioStreamP constant
 
---type FrameSignal = Eff (dom :: DOM, timer :: TIMER) (Signal Time)
 
---main :: forall t19. Eff ( timer :: TIMER, console :: CONSOLE | t19) Unit
-main = onDOMContentLoaded do
-	ctx <- getAudioContext
-	analyser <- getAnalyserNode ctx
-	inputStream <- microphoneInputFrame ctx
-	let stringified = map show inputStream
-	runSignal (map log stringified)
-  	--frame <- animationFrame
-  	--runSignal (map log (map show frame))
+main = do
+  log "hey"
+  audio <- audioStream
+  runSignal $ map log audio
